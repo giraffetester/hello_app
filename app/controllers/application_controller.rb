a8654024349189e4c874c1aeca7a5761cf0ab471
mghtest <qitmadagascar@gmail.com>
2015-09-28 14:42:50 -0700

    Change greeting language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "hello, world!"
+  	render text: "¡hola, mundo!"
   end
 end
